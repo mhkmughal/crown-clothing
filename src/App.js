cedf953db442d6478a4f1667ae5a00024a55d028
Hamza khan <hamzakhan2901@gmail.com>
2019-10-21 15:02:01 -0400

    Project-Setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HomePage from './homepage.component';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Crown Clothing</h1>
+      <HomePage/>
+    </div>
+  );
+}
+
+export default App;
