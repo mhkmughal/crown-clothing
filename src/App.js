915748992932f002cbc80531552dd8f67562f75f
Hamza khan <hamzakhan2901@gmail.com>
2019-10-22 12:20:21 -0400

    Component Structuring

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import HomePage from './components/pages/homepage/homepage.component';
 import './App.css';
 
 function App() {
-  const mainStyles={
-    marginLeft:'43%'
+  const mainStyles = {
+    marginLeft: '43%'
   }
   return (
     <div className="App">
       <h1 style={mainStyles}>Crown Clothing</h1>
-      <HomePage/>
+      <HomePage />
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import './menu-item.style.scss'
 
-const MenuItem = ({title}) => (
-
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">{title}</h1>
-                    <h1 className="title">SHOP NOW</h1>
-                </div>
+const MenuItem = ({ title, imageUrl, size }) => (
+    <div 
+    style={{ 
+        backgroundImage:`url(${imageUrl})`}} 
+        className="menu-item"    
+    >
+            <div className="content">
+                <h1 className="title">{title}</h1>
+                <h1 className="title">SHOP NOW</h1>
             </div>
-); 
+        </div>
+);
 export default MenuItem;
