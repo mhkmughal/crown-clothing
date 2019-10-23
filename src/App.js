687a2770ed73c10dac5b54f3ef5c3c8c1486440c
Hamza khan <hamzakhan2901@gmail.com>
2019-10-23 16:09:23 -0400

    Navbar Design

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import HomePage from './components/pages/homepage/homepage.component';
 import './App.css';
-import {Route} from 'react-router-dom';
-import ShopPage from './components/pages/shop/shop.component'
+import {Switch, Route} from 'react-router-dom';
+import ShopPage from './components/pages/shop/shop.component.jsx';
+import Header from './components/header/header.component'
 
 function App() {
-  const mainStyles = {
-    textAlign: 'center'
-  }
   return (
     <div className="App">
-      <h1 style={mainStyles}>Crown Clothing</h1>
-      <switch>
+      <Header/>
+      <Switch>
       <Route exact path='/' component={HomePage} />
        <Route exact path='/shop' component={ShopPage} />
-       </switch>
+       </Switch>
     </div>
   );
 }
