9220231ac08bd3afa3acc56b28216e55efc445d0
Hamza khan <hamzakhan2901@gmail.com>
2019-10-23 15:43:34 -0400

    Shop Page and Preview

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import HomePage from './components/pages/homepage/homepage.component';
 import './App.css';
 import {Route} from 'react-router-dom';
+import ShopPage from './components/pages/shop/shop.component'
 
 function App() {
   const mainStyles = {
@@ -12,8 +13,8 @@ function App() {
       <h1 style={mainStyles}>Crown Clothing</h1>
       <switch>
       <Route exact path='/' component={HomePage} />
-{/*       <Route exact path='/hats' component={Hats Page} />
- */}      </switch>
+       <Route exact path='/shop' component={ShopPage} />
+       </switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './menu-item.style.scss';
-import {withRouter} from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 
 const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
     <div
-    className=  {`${size} menu-item`} onClick={()=> history.push(`${match.url}${linkUrl}`)}> 
-    <div
-    className="background-image"
-    style={{ backgroundImage: `url(${imageUrl})`}}
-/>
+        className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
+        <div
+            className="background-image"
+            style={{ backgroundImage: `url(${imageUrl})` }}
+        />
         <div className="content">
             <h1 className="title">{title}</h1>
             <h1 className="title">SHOP NOW</h1>
